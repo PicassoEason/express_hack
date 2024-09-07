63fdc51a17c1cdd47d33fdcc6986a4742dcb2641
picassoeason <easonwu.student@outlook.com>
2024-09-07 14:10:42 +0800

    add package.json

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 5001;
 
 // 使用 /food 路徑來處理所有食品相關的請求
 app.use('/food', foodRouter);
-
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
